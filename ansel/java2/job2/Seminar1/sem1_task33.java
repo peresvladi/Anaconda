0d5a24817650193976ab27275f081c67a1b2a569
peresvladi <peresvladi@gmail.com>
2023-01-31 23:28:34 +0300

    /home/vladi/Education/master/ansel/java2/job2/Seminar1/sem1_task33.java

--- ansel/java2/job2/Seminar1/sem1_task33.java
+++ ansel/java2/job2/Seminar1/sem1_task33.java
@@ -1,21 +1,35 @@
 package Seminar1;
 import java.util.Scanner;
-public class sem1_task33 {public static void main (String[]args) {
-	prtArray(new int [] {1, 2, 3, 4, 54});
-	
-	}
+public class sem1_task33{
+     public static void main(String[] args) {
+     int countt = ArrLen();
+     int[] arr = new int[countt];
+     for (int i = 0; i <= countt; i++)
+     arr[i] = (int) ( Math.random() * 2 ); 
+     check(arr);
+      }
+        
+            private static void check(int[] arr) {
+               for (int i = 0; i < arr.length; i++){
+                    System.out.println(arr[i]);
+               }
 
 
-     
-     
-public static void prtArray (int[]array) {
-	for (int i = 0; i < array.length; i++)
-	System.out.print(array[i] + " ");
-}
+     }
 
 
-}	
-
+          static int ArrLen(){
+           Scanner iScanner = new Scanner (System.in);
+           System.out.println("Введите колличество элементов массива: ");
+           int countt = iScanner.nextInt();
+           return countt;
+       }
+       
+       
+      
+           
+       
+   }
 
 
 
