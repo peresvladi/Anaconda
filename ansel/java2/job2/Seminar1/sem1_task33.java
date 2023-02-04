f0de2c51cddd7bd8519454703025546ab5aa360b
peresvladi <peresvladi@gmail.com>
2023-02-04 21:52:34 +0300

    /home/vladi/Education/master/ansel/java2/job2

--- ansel/java2/job2/Seminar1/sem1_task33.java
+++ ansel/java2/job2/Seminar1/sem1_task33.java
@@ -1,27 +1,46 @@
+/* Дан массив двоичных чисел, вывести максимальное количество идущих подряд 1. */
 package Seminar1;
 import java.util.Scanner;
 public class sem1_task33{
      public static void main(String[] args) {
      int countt = ArrLen();
      int[] arr = new int[countt];
-     for (int i = 0; i <= countt; i++)
+     for (int i = 0; i < countt; i++)
      arr[i] = (int) ( Math.random() * 2 ); 
      check(arr);
       }
-        
-            private static void check(int[] arr) {
-               for (int i = 0; i < arr.length; i++){
-                    System.out.println(arr[i]);
-               }
+                   private static void check(int[] ar) {
+               int ix = 0;
+               int iy = 0;
+               for (int i = 0; i < ar.length; i++){
+               System.out.println("- "+ar[i]);
+                     if (ar[i]==1 && i < ar.length){
+                         ix += 1;
+               }else{
+                         int max = ix > iy ? iy = ix : ix;
+                         ix = 0;
+                         if (i == ar.length-1){
+                         System.out.println(max);          
+                         }
+                         
+                }
+                
 
 
-     }
 
+               
+                         }                        
+                    }
 
+                    
+               
+               
+         
           static int ArrLen(){
-           Scanner iScanner = new Scanner (System.in);
+           /* Scanner iScanner = new Scanner (System.in);
            System.out.println("Введите колличество элементов массива: ");
-           int countt = iScanner.nextInt();
+           int countt = iScanner.nextInt(); */
+           int countt = 5;
            return countt;
        }
        
@@ -29,7 +48,7 @@ public class sem1_task33{
       
            
        
-   }
+   } 
 
 
 
