cfc00264775b44424611cf66bfa574d87056527c
peresvladi <peresvladi@gmail.com>
2023-02-27 10:36:43 -0500

    vladi@vladi-x540y:~/Education/master/ansel$ dz6 18-36 27-02-2023

--- ansel/job2/practical_task2/task6/computer_store/comp_store.java
+++ ansel/job2/practical_task2/task6/computer_store/comp_store.java
@@ -4,11 +4,19 @@ public class comp_store {
 int Id;
 String Manuf_r;
 String Mod_name;
-String RAM_type;
 int RAM_vol;
 String HD_type;
 int HD_vol;
+String colour;
 String OS_manuf_r;
 String OS_name;
 Double Price;
+
+@Override
+public String toString() {
+    String res = String.format(Id, 
+    HD_type,
+     null)
+}
+
 }
--- ansel/job2/practical_task2/task6/computer_store/control_details.java
+++ ansel/job2/practical_task2/task6/computer_store/control_details.java
@@ -5,9 +5,57 @@ public class control_details {
        comp_store c1 = new comp_store();
        c1.Manuf_r = "Lenovo";
        c1.Mod_name = "GA-MA790X-UD3P";
-       c1.RAM = "DDR4 3200 MHz 16G";
-       c1.OS = "Майкрософт Windows 10 Домашняя";
-         
+       c1.RAM_vol = 16;
+       c1.HD_type = "SSD";
+       c1.HD_vol = 512;
+       c1.colour = "Silver";
+       c1.OS_manuf_r = "Microsoft";
+       c1.OS_name =  "Windows 10 Home";
+       c1.Price = 53710.0;
+
+       comp_store c2 = new comp_store();
+       c2.Manuf_r = "Asus";
+       c2.Mod_name = "D1500CEPE-DQ0756";
+       c2.RAM_vol = 16;
+       c2.HD_type = "SSD";
+       c2.HD_vol = 512;
+       c2.colour = "Black";
+       c2.OS_manuf_r = "without an";
+       c2.OS_name =  "without an";
+       c2.Price = 59510.0;
+
+       comp_store c3 = new comp_store();
+       c3.Manuf_r = "Asus";
+       c3.Mod_name = "VT500QC-HN058";
+       c3.RAM_vol = 16;
+       c3.HD_type = "SSD";
+       c3.HD_vol = 512;
+       c3.colour = "Black";
+       c3.OS_manuf_r = "Microsoft";
+       c3.OS_name =  "Windows 10 Home";
+       c3.Price = 57000.0;
+
+       comp_store c4 = new comp_store();
+       c4.Manuf_r = "Acer Aspire";
+       c4.Mod_name = "7A715-42G-R6VJ";
+       c4.RAM_vol = 8;
+       c4.HD_type = "SSD";
+       c4.HD_vol = 256;
+       c4.colour = "Black";
+       c4.OS_manuf_r = "Microsoft";
+       c4.OS_name =  "Windows 10 Home";
+       c4.Price = 59900.0;
+
+       comp_store c5 = new comp_store();
+       c5.Manuf_r = "MSI";
+       c5.Mod_name = "GF 66 KATANA 11SC-1251XPU";
+       c5.RAM_vol = 8;
+       c5.HD_type = "SSD";
+       c5.HD_vol = 256;
+       c5.colour = "Black";
+       c5.OS_manuf_r = "Microsoft";
+       c5.OS_name =  "Windows 10 Home";
+       c5.Price = 55.28;
     }
     
 }
