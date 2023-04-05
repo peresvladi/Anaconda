482de43eea4d5cc2fb9248a94a1ca82f03f5ee65
vladiperes <peresvadi@mail.ru>
2023-04-05 13:03:32 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   OOP/komp1/Seminar_komp1/S3_komp1/my/S3_komp2/Main.java 	new file:   OOP/komp1/Seminar_komp1/S3_komp1/my/S3_komp2/Personal.java 	new file:   OOP/komp1/Seminar_komp1/S3_komp1/my/S3_komp2/User.java  Untracked files: 	"../\320\224\320\273\321\217\320\257\320\275\320\264\320\224\320\270\321\201\320\272/" comp1 05-04-23 13:07

---
+++ ansel/OOP/komp1/Seminar_komp1/S3_komp1/my/S3_komp2/Main.java
@@ -0,0 +1,10 @@
+package S3_komp2;
+
+public class Main {
+    public static void main(String[] args) {
+    User user1 = new User("Иванов", "Иван", 27);
+
+   System.out.println(user1);
+    }
+   
+}
